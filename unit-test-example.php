f1e5ecd60da041c69153fd47e0f392f96fea754d
Andrew Lima <andrew@arctek.co.za>
2018-05-16 14:57:51 +0200

    Initial Commit
    This is just an initial commit.

---
+++ unit-test-example.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Unit Test Example
+ * Description: This plugin is just used to solely test Travis-CI and WordPress.
+ * Plugin URI: https://somelink.com
+ * Author: An Author's Name
+ * Author URI: https://somelink.com
+ * Version: 1.0
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: Text Domain
+ * Domain Path: Domain Path
+ * Network: false
+ */
+
+defined( 'ABSPATH' ) or exit;
+
+function my_shortcode() {
+	echo 'Some shortcode output.';
+}
+add_shortcode( 'my_shortcode', 'my_shortcode' );
